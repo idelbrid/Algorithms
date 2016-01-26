04dcbd501957352dd695e11b5d7f619eb77c7f98
Ian Delbridge <idelbrid@phelps.csug.rochester.edu>
2016-01-25 21:14:27 -0500

    Serous changes to add events in

--- kwise_ind/kwise_ind.py
+++ kwise_ind/kwise_ind.py
@@ -15,6 +15,42 @@ def read_input():
             rv_data[i-1][:] = line.split(' ')
     return rv_data
 
+class event:
+    next_id = 0
+    cached_ints = dict()
+    ind_events = dict()
+    
+    def __init__(self, atomic_events=None, intersected_events=None, 
+                 intersected_events_ids=None):
+        self.atomic_events = atomic_events
+        self.size = len(atomic_events)
+        self.id = event.next_id
+        event.next_id += 1        
+        if intersected_events is None:
+            self.intersected_events = frozenset([self])
+        else:
+            self.intersected_events = intersected_events
+        if intersected_events_ids is None:
+            self.intersected_events_ids = frozenset([self.id])
+        else:
+            self.intersected_events_ids = intersected_events_ids
+            
+    def cached_intersect(e1, e2):
+        total_intersections = e1.intersected_events_ids.union(
+                                    e2.intersected_events_ids)
+        if total_intersections in event.cached_ints:
+            return event.cached_ints[total_intersections]
+        else:
+            int_at_evts = e1.atomic_events.intersection(e2.atomic_events)
+            total_events = e1.intersected_events.intersection(e2.intersected_events)            
+            e3 = event(int_at_evts, total_events, total_intersections)
+            return e3
+            
+    def intersect(self, other):
+        intersection = event.cached_intersect(self, other)
+        return intersection
+        
+
 def print_input(rv_data):
     print len(rv_data[1]), len(rv_data)
     for row in rv_data:
@@ -31,80 +67,78 @@ def find_rv_inverses(rv_data):
            else:
                d[value] = set([i])
        for key in d:
-           d[key] = frozenset(d[key])
+           d[key] = event(frozenset(d[key]))
        rv_inverse[rv_index] = d
     return rv_inverse
-    
-huge_intersection_cache = dict()
-def intersect_cached(sets, new_set):
-    S = sets.union(frozenset([new_set])) # set of sets being intersected
-    if S in huge_intersection_cache:
-        return huge_intersection_cache[S]
-    else:
-        huge_intersection_cache[S] = huge_intersection_cache[sets].intersection(new_set)
-        return huge_intersection_cache[S]
-        
-    
+
+
 class state:
     """
     k = number of rv being considered at one time
     parent = previously computed state
     children = next states computed
-    prior_sets = set of sets known to be independent
-    posterior = set testing to be independent of the prior
-    joint = set of prior intersect posterior
+    prior_event = intersection of independent events
+    posterior = event testing to be independent of the prior
+    joint = prior intersect posterior
     rv_used = the random variables being considered in this branch
     """
     space_size = -1
-    def __init__(self, original=None, new_set=None, rv_of_new=None):
+    def __init__(self, original=None, new_event=None, rv_of_new=None):
         self.parent = original
         if original is not None:
             original.children.append(self)
-            if original.k == 1:
-                self.prior = original.posterior  
-            else:
-                self.prior = original.joint
+            self.prior = original.joint
             self.rv_used = original.rv_used + [rv_of_new]
-            self.joint = None
+            self.joint = None  # to be filled in later if explored
             self.k = original.k + 1
         else:
-            self.prior = frozenset([])
+            self.prior = None
             self.rv_used = [rv_of_new]
-            self.joint = new_set
+            self.joint = new_event
             self.k = 1
-        self.posterior = new_set
+        self.posterior = new_event
         self.children = list()
 
     def check_independence(self):
         if self.parent is None:
+            self.joint.ind = True
             return True
-        self.joint = self.prior.intersection(self.posterior)
-        return len(self.joint) * state.space_size == len(self.parent.joint) * len(self.posterior)
+        self.joint = self.prior.intersect(self.posterior)
+        ind = self.joint.size * state.space_size == self.prior.size * self.posterior.size
+        self.joint.ind = ind
+        return ind
         
     def successors(self):
         suc = list()
+        if self.joint.intersected_events_ids in event.ind_events:
+            return suc # don't move forward if you'd already explored this state
+        event.ind_events[self.joint.intersected_events_ids] = self.joint.ind
+
         for rv_index, rv in enumerate(rv_inverse):
             if rv_index not in self.rv_used:  # can't compare X=1 ind? X=2
                 for value in rv:
                     suc.append(state(self, rv[value], rv_index))
         return suc
 
-rv_data = read_input()
-# rv_data = np.array([[1,1,1,1,0,0,0,0],
-#                    [1,1,0,0,1,1,0,0],
-#                    [1,0,1,0,1,0,1,0]])
+# rv_data = read_input()
+#rv_data = np.array([[1,1,1,1,0,0,0,0],
+#                   [1,1,0,0,1,1,0,0],
+#                   [1,0,1,0,1,0,1,0]])
+import pandas as pd
+df = pd.read_csv('rv.csv')
+rv_data = np.asarray(df.as_matrix())
 state.space_size = len(rv_data[0])        
 print_input(rv_data)
 
 rv_inverse = find_rv_inverses(rv_data)
 
 q = Queue()
-for rv_index, rv in enumerate(rv_inverse):
+for rv_index, rv in enumerate(rv_inverse):  #initialize the queue
     for val in rv:
-        q.put(state(new_set=rv[val], rv_of_new=rv_index))
+        q.put(state(new_event=rv[val], rv_of_new=rv_index))
         
 max_k = len(rv_data)
-while not q.empty():
+while not q.empty():  
     s = q.get()
     if not s.check_independence():
         max_k = s.k-1
