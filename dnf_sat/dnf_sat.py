39de5f442bbd75edb1dbdb1f43428c7927460b96
Ian Delbridge <idelbrid@u.rochester.edu>
2016-02-09 21:28:32 -0500

    fixed reading from file issues and wrote readme and writeup.

--- dnf_sat/dnf_sat.py
+++ dnf_sat/dnf_sat.py
@@ -1,7 +1,7 @@
 import sys
 import numpy as np
 
-# np.random.seed(123456789)
+np.random.seed(123456789)
 
 def read_input():
     n, m = -1, -1
@@ -11,17 +11,20 @@ def read_input():
 
     for i, line in enumerate(sys.stdin):
         if i == 0:
-            n, m = line.split(' ')
-            dnf = np.zeros((int(m), int(n)))
-        elif i < m - 2:
+            nandm = line.split(' ')
+            n = int(nandm[0])
+            m = int(nandm[1])
+            dnf = np.zeros((int(m), int(n)), dtype=np.int8)
+        elif i <= m:
             for word in line.split(' '):
                 j = np.abs(int(word))
                 sign = int(word) / j
                 dnf[i-1][j-1] = sign
-        elif i < m - 1:
-            precision = float(line.split(' '))
+        elif i < m + 1:
+            precision = float(line.split(' ')[0])
         else:
-            error_prob = float(line.split(' '))
+            error_prob = float(line.split(' ')[0])
+
     return dnf, precision, error_prob
 
 def sample_choice_helper(dnf, space_size):
@@ -30,12 +33,12 @@ def sample_choice_helper(dnf, space_size):
 
     
     clause_indx = -1
-    random_choice = np.random.randint(0, space_size)  #which state
+    random_choice = np.random.randint(0, space_size)  #randomly choose which state
     while random_choice >= 0:
         clause_indx += 1
         clause_freedom_size = 2 ** (num_vars - np.abs(dnf[clause_indx]).sum())
         random_choice -= clause_freedom_size
-    # clause_indx -= 1
+
     clause = np.copy(dnf[clause_indx])
     random_choice += 2 ** (num_vars - np.abs(dnf[clause_indx]).sum())
     
@@ -76,24 +79,30 @@ def find_space_size(dnf):
     return clause_options.sum()
 
 if __name__ == "__main__":
-    # F, epsilon, delta = read_input()
+    F, epsilon, delta = read_input()  # real input
+
     # F = np.array([[1, 1, 1], [-1, -1, -1]])  # fake input 1
+
     # F = np.array([[1, 0, -1], [1, 1, 0]])  # fake input 2
-    F = np.array([[1, 1, 1, 1 , 0, 0, 0, 0, -1], 
-                  [-1, 1, 1, 1, -1, 1, 1, 1, 1]])
-    epsilon = 0.1
-    delta = 0.01
+
+    # F = np.array([[1, 1, 1, 1 , 0, 0, 0, 0, -1], #fake input 4
+    #              [-1, 1, 1, 1, -1, 1, 1, 1, 1],
+    #              [1, 1, 1, 1, 0, 0, 0, 0, 1]])
+
     n, m = len(F), len(F[0])
-    num_samples = 100 * int(- m * 1/epsilon ** 2 * np.log(delta))
+    num_samples = int(-1 * m * 1/epsilon ** 2 * np.log(delta))
     space_size = find_space_size(F)
     
     # tests = np.zeros(num_samples)
-    # tests = batch_sample(tests, F)  # 2ill not work... vectorize goes with F as the array
+    # h = lambda x: sample(F, space_size)    # batch test
+    # hvec = np.vectorize(h)                 # slower
+    # tests = hvec(tests)
+    # s = tests.sum()
+    
     cumulative = 0    
     for indx in range(0, num_samples):  # One by one test...
         cumulative += sample(F, space_size)
-    satisfiable_ratio = cumulative / float(num_samples)  # ratio of clauses that were satisfiable
-    print num_samples
+        
+    satisfiable_ratio = cumulative / float(num_samples)  # ratio 
+
     print satisfiable_ratio * space_size  # scale ratio by total sample space size
-
-
